73972bfa9798d0687a60818922f74eb821142846
sebas <se.chacon@estudiantec.cr>
2024-10-05 00:47:49 -0600

    Bitácora InsercionEmpleado
    Se mejoró tanto el sp de insertar-empleado como su funcionalidad en app.py para registrar correctamente tanto errores como eventos en sus respectivas tablas

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash,sess
 import pyodbc
 from datetime import datetime, timedelta
 
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
